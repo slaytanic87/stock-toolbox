70ba703b0d53c2418535b936838747d879ac6d96
Anon <le872002@yahoo.de>
2021-02-01 21:14:03 +0100

    create stock calendar scraper

--- stock-proxy/boersennews/index.js
+++ stock-proxy/boersennews/index.js
@@ -19,19 +19,45 @@ function fetchNews(res) {
 }
 
 function fetchCalendar(res) {
+    let calendarArr = [];
     axios.get(calendar).then((resp) => {
         const dom = cheerio.load(resp.data);
         let rows = dom("div[class=row]").find("table[id=calendarTable] > tbody > tr");
         rows.each((index, elem) => {
-            let dayElem = dom(elem).find("span[class=text-white]").text();
-        })
+            let day = dom(elem).find("span[class=text-white]").text();
+            let event = dom(elem).find("strong[class=text-black]").text();
+            let countriesArr = getCountryHeader(dom, dom(elem).find("tr[class='small text-center'] > th > span"));
+            let statusArr = getStatus(dom, dom(elem).find("tr[class=text-center] > td > i"));
+            calendarArr.push({
+                day: day,
+                event: event,
+                countries: countriesArr,
+                status: statusArr
+            });
+        });
+        res.json(calendarArr);
         res.end();
     }).catch((err) => {
        console.log(err);
-       res.error();
+       res.end();
     });
 }
 
+function getStatus(dom, rows) {
+    let status = [];
+    rows.each((index, elem) => {
+        status.push(dom(elem).attr("title"));
+    });  
+    return status;
+}
+
+function getCountryHeader(dom, rows) {
+    let countries = [];
+    rows.each((index, elem) => {
+        countries.push(dom(elem).text());
+    });
+    return countries;
+}
 
 function callNews(url, newsList, category) {
     return new Promise((resolve, reject) => {
